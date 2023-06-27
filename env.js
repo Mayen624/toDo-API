77146e4d0bacdd0496878533f4d76e41bfd063c7
Mayen624 <juliomayen300@gmail.com>
2023-06-27 01:51:45 -0300

    Some shh... changes realized.

--- env.js
+++ env.js
@@ -2,5 +2,5 @@ export const env = {
     PORT: 3000,
     DB_USER: 'Mayen624',
     DB_NAME: 'api',
-    DB_PASS: 'ggqZyCy4S68qKouX'
+    DB_PASS: 'Idd370qbu/2mgN2NsfIYkWT+0pdAVqdjGjJOSa/QUus=' // {MYN.DEV: devglan.com}
 }
